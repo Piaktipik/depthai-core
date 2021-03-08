5bd735e9e07670795097db0a3ec4191a3afe588d
SzabolcsGergely <szabi@luxonis.com>
2021-03-08 22:47:01 +0200

    Temporary workaround for issues with NN shapes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ae1c9ddcbd69ecbf9f6aeb720f3158a3003a8b3a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0937669f97453093da5c308c4ea1e8ce4b39dff1")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
